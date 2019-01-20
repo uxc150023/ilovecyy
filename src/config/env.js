8edcd60fa729f18cc44ea738d548a2d915d978d1
chengyunyun <chengyunyun@xinxuejie.cn>
2019-01-20 16:05:00 +0800

    config

--- src/config/env.js
+++ src/config/env.js
@@ -13,9 +13,9 @@ let imgBaseUrl = '';
 
 
 if (process.env.NODE_ENV == 'development') {
-	// baseUrl = 'https://console.xinxuejie.com.cn/';
+	baseUrl = 'https://console.xinxuejie.com.cn/';
     // baseUrl = 'http://192.168.31.188:80/';
-    baseUrl = 'http://192.168.31.116:80/';
+    // baseUrl = 'http://192.168.31.116:80/';
     // imgBaseUrl = '/img/';
 
 }else if(process.env.NODE_ENV == 'production'){
