054794cbe9455b521c4353cd09261587ef6884c8
chengyunyun <chengyunyun@xinxuejie.cn>
2019-02-23 15:12:18 +0800

    1

--- src/config/env.js
+++ src/config/env.js
@@ -15,8 +15,7 @@ if (process.env.NODE_ENV === 'development') {
     // baseUrl = 'https://console.xinxuejie.com.cn/'
     // baseUrl = 'http://192.168.31.188:80/'
     // baseUrl = 'http://192.168.31.116:80/'
-    baseUrl = 'http://eqk39k.natappfree.cc/'
-    // imgBaseUrl = '/img/';
+    baseUrl = 'http:///wcekfr.natappfree.cc/'
 } else if (process.env.NODE_ENV === 'production') {
     baseUrl = 'https://console.xinxuejie.com.cn/'
     // imgBaseUrl = '//elm.cangdu.org/img/';
